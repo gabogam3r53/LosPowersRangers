0c61f67724762df849df53e092db642aaa78b17e
DanielaCanasU <danielacanasu@gmail.com>
2024-11-15 19:47:53 -0400

    Datos del 2024 al 2016

--- Analisis_Datos/Correlaciones.py
+++ Analisis_Datos/Correlaciones.py
@@ -5,21 +5,19 @@ años = [2024,2023,2022,2021,2020]
 def filtrar_datos(df,año_contrato,año_estadisticas):
     df_correlacion = df['Contrato {}'.format(año_contrato)]
     df_año_estadisticas = df.filter(like= str(año_estadisticas))
-    print(df_año_estadisticas)
     df_año_estadisticas.drop('Contrato {}'.format(año_estadisticas), axis=1, inplace = True)
     df_año_estadisticas.drop('Equipo {}'.format(año_estadisticas), axis=1, inplace = True)
-    print(df_año_estadisticas)
     df_correlacion = pd.concat([df_correlacion, df_año_estadisticas],axis = 1)
     return df_correlacion
 
 #Importar datos
-df = pd.read_csv('datos_completos.csv')
-df.columns = df.columns.str.strip()
-df.columns = df.columns.str.replace('\t', ' ')
+df = pd.read_csv('datos_completos_2023.csv')
+#df.columns = df.columns.str.strip()
+#df.columns = df.columns.str.replace('\t', ' ')
 #correlacion = df[['Contrato 2024', 'RANK 2023', 'AGE 2023', 'GP 2023', 'W 2023', 'L 2023', 'MIN 2023', 'PTS 2023', 'FGM 2023', 'FGA 2023', 'FG% 2023', '3PM 2023', '3PA 2023', '3P% 2023', 'FTM 2023', 'FTA 2023', 'FT% 2023', 'OREB 2023', 'DREB 2023', 'REB 2023', 'AST 2023', 'TOV 2023', 'STL 2023', 'BLK 2023', 'PF 2023', 'FP 2023', 'DD2 2023', 'TD3 2023', '+/- 2023']].corr()
 #correlacion.to_csv('Correlacion.csv')
 
-
+df.info(verbose=False)
 
 for i in range(len(años) - 1):
     df_correlacion = filtrar_datos(df,años[i],años[i + 1])
@@ -30,5 +28,5 @@ for i in range(len(años) - 1):
         contrato_correlacion = i_correlacion
     else:
         contrato_correlacion = pd.concat([contrato_correlacion,i_correlacion],axis = 1)
-#contratos_correlacion.to_csv('Correlacion_contrato_2022_2021.csv')
-print(contrato_correlacion)
+contrato_correlacion.to_csv('Correlacion_contrato.csv')
+#print(contrato_correlacion)
