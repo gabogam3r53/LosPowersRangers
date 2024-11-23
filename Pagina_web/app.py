37c9dea6298105e0a78851c32389bbccbe71e2f8
DanielaCanasU <danielacanasu@gmail.com>
2024-11-23 13:56:41 -0400

    abrev español app.py

--- Pagina_web/app.py
+++ Pagina_web/app.py
@@ -18,37 +18,30 @@ def main():
     if data is None:
         st.error("Failed to load data. Please try again later.")
         return
-    abbreviations = {
-        "GP": "Games Played",
-        "W": "Wins",
-        "L": "Losses",
-        "MIN": "Minutes Played",
-        "FGM": "Field Goals Made",
-        "FGA": "Field Goals Attempted",
-        "FG%": "Field Goal Percentage",
-        "3PM": "3 Point Field Goals Made",
-        "3PA": "3 Point Field Goals Attempted",
-        "3P%": "3 Point Field Goals Percentage",
-        "FTM": "Free Throws Made",
-        "FTA": "Free Throws Attempted",
-        "FT%": "Free Throw Percentage",
-        "OREB": "Offensive Rebounds",
-        "DREB": "Defensive Rebounds",
-        "REB": "Rebounds",
-        "AST": "Assists",
-        "TOV": "Turnovers",
-        "STL": "Steals",
-        "BLK": "Blocks",
-        "PF": "Personal Fouls",
-        "FP": "Fantasy Points",
-        "DD2": "Double doubles",
-        "TD3": "Triple doubles",
-        "PTS": "Points",
-        "+/-": "Plus Minus"
-    }
 
-    # Create a table using st.table()
-    st.table(pd.DataFrame.from_dict(abbreviations, orient='index', columns=['Meaning']))
+    # Leyenda
+    abbrev = pd.DataFrame.from_dict(ABBREVS_WNBA, orient='index', columns=['Meaning'])
+
+    st.markdown("""
+        <style>
+        .stTable td {
+            border: 1px solid #ddd;
+            padding: 8px;
+            text-align: left;
+        }
+        .stTable th {
+            background-color: #f2f2f2;
+            border: 1px solid #ddd;
+            padding: 8px;
+            text-align: left;
+        }
+        </style>
+        """)
+
+    # Mostrar la tabla con los estilos aplicados
+    st.table(abbrev)
+
+    
     # Crea tres secciones principales usando columnas
     col1, col2 = st.columns([2, 1])
 
