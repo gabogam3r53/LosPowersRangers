cdc44451a740d345f9306ccc132980b2b6d6ae32
DanielaCanasU <danielacanasu@gmail.com>
2024-11-24 14:14:01 -0400

    Correlacion tabla arreglo 2

--- Pagina_web/config.py
+++ Pagina_web/config.py
@@ -3,7 +3,7 @@ DATA_URL = f"{GITHUB_BASE_URL}/Datos/datos_todas_las_jugadoras_posibles.xlsx"
 CONTRACT_GRAPH_URL = f"{GITHUB_BASE_URL}/Analisis_Datos/Graficas/Contratos_contra_estadisticas_interesantes_todos_mas_datos.png"
 INDIVIDUAL_GRAPHS_DIR = "https://raw.githubusercontent.com/gabogam3r53/PowersRangers/refs/heads/develop/Analisis_Datos/Graficas/Graficas_datos_individuales/"
 YEARLY_GRAPHS_DIR = "https://raw.githubusercontent.com/gabogam3r53/PowersRangers/refs/heads/develop/Analisis_Datos/Graficas/Graficas_datos_liga_por_año/"
-DATA_CORRELACION_URL = "https://github.com/gabogam3r53/PowersRangers/blob/develop/Analisis_Datos/Tablas/Correlacion_por_a%C3%B1o.xlsx"
+DATA_CORRELACION_URL = "https://github.com/gabogam3r53/PowersRangers/raw/develop/Analisis_Datos/Tablas/Correlacion_por_a%C3%B1o.xlsx"
 
 STATS_COLUMNS = [
     'RANK', 'AGE', 'GP', 'MIN', 'PTS', 'FGM', 'FGA', 'FG%', '3PM', '3PA', '3P%',
