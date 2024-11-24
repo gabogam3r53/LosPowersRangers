6bdb831798bd000ac60759e133a2a249d6b1c37b
Gabo <153545757+gabogam3r53@users.noreply.github.com>
2024-11-24 14:21:39 -0400

    Observacion Natasha Howard app.py

--- Pagina_web/app.py
+++ Pagina_web/app.py
@@ -99,7 +99,9 @@ def main():
                 Nota:Es importante destacar que en las gráficas individuales, estas correlaciones se visualizan claramente con tendencias ascendentes.
                 * **Correlaciones Negativas:** RANK (Ranking de Jugadoras) y TOV (Pérdidas de Balón) presentan una correlación negativa con los valores de los contratos. Las jugadoras con mejor ranking (número más bajo) y las jugadoras con menos pérdidas de Balón tienden a recibir contratos más altos.
                 * **Falta de Correlación Aparente en Algunas Estadísticas:** Algunas estadísticas como AGE (Edad) muestran una correlación positiva o negativa pero menos significativa, lo cual podría indicar que este factor no es tan determinante en la valoración de los contratos.
-
+                 * **Observacion:**Hay algunas jugadoras como por ejemplo "Natasha Howard", las cuales si se fijan en la grafica de puntos, tuvieron una mejora al pasar los años(primer contrato) y en consecuencia obtuvieron un mejor contrato debido a que las Fichó un nuevo equipo, ahora bien, este cambio hace que el numero de puntos que estaba teniendo anteriormente tienda a disminuir mientras se acostumbra a sus nuevas compañeras 
+                y logre adaptarse para tener las mismas posibilidades de hacer puntos en su nuevo equipo.
+                                    
                 **Conclusiones:**
                 * **Factores de Rendimiento Individual:**  
                 El rendimiento individual medido en puntos, intentos y tiros de campo anotados, minutos jugados, puntos de fantasía y porcentaje de tiros de tres puntos tiene una influencia significativa en la determinación del valor de los contratos de las jugadoras.
