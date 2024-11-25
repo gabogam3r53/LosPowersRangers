ae3d633e68252e2187087863507791a0ea11dfb8
DanielaCanasU <danielacanasu@gmail.com>
2024-11-24 23:25:21 -0400

    Color titulo app.py

--- Pagina_web/app.py
+++ Pagina_web/app.py
@@ -19,7 +19,7 @@ def main():
     </style>
     """
     st.markdown(estilo, unsafe_allow_html=True)
-    st.title("Estadísticas de Jugadoras de la WNBA (2016-2024)")
+    st.markdown(":blue[ ### Estadísticas de Jugadoras de la WNBA (2016-2024)]")
     st.write("El presente proyecto se enfoca en el ámbito del baloncesto femenino de la WNBA, recopilando datos exhaustivos de las temporadas 2016-2024 directamente de las fuentes oficiales: Spotrac.com y Stats.wnba.com. Este conjunto de datos permitió realizar un análisis detallado de las jugadoras, sus estadísticas y tendencias a lo largo de los años.")
 
     # Cargar datos
