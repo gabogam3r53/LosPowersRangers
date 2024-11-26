a76b451ad47292bbfe7d45679d593ad7689a4069
Gabo <153545757+gabogam3r53@users.noreply.github.com>
2024-11-25 21:58:07 -0400

    test70 app.py

--- Pagina_web/app.py
+++ Pagina_web/app.py
@@ -19,7 +19,7 @@ background-image: url({background_image_url});
 background-size: contain; 
 background-position: 100%;
 background-repeat: no-repeat;  
-background-attachment: local;
+background-attachment: fixed;
 background-color: #000000;
 }} 
 </style> 
