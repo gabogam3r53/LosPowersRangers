e2b84e4e74c4d24d8f990af3f02be79fc09331d3
Gabo <153545757+gabogam3r53@users.noreply.github.com>
2024-11-25 21:43:37 -0400

    test69 app.py

--- Pagina_web/app.py
+++ Pagina_web/app.py
@@ -29,8 +29,6 @@ st.markdown(page_bg_img, unsafe_allow_html=True)
 
 
 def main():
-    bg = load_image(background_image_url)
-    st.image(bg, use_column_width=True)
     
     st.markdown("# :blue[Estadísticas de Jugadoras de la WNBA (2016-2024)]")
     st.write("El presente proyecto se enfoca en el ámbito del baloncesto femenino de la WNBA, recopilando datos exhaustivos de las temporadas 2016-2024 directamente de las fuentes oficiales: Spotrac.com y Stats.wnba.com. Este conjunto de datos permitió realizar un análisis detallado de las jugadoras, sus estadísticas y tendencias a lo largo de los años.")
